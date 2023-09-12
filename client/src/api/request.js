aae407cf460b3963b3456b7192a63c7f9f30679f
d-yx <352104389@qq.com>
2023-09-12 14:35:28 +0800

    feat:add request func

---
+++ client/src/api/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://localhost:5173',
+  timeout: 5000
+})
+
+// 设置请求拦截
+request.interceptors.request.use((config) => {
+  // config就是你的请求
+  // 做一些其他的事情。。。
+  // eg:config.headers=...
+
+  // 请求放行
+  return config
+})
+
+
+// 设置响应拦截
+request.interceptors.response.use((response) => {
+  // 拦截的响应
+  // 对响应进行处理
+
+  // 响应放行
+  return response
+}, (err) => {
+  //对错误进行一个处理
+  return Promise.reject(err)
+})
